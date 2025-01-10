31d2a68142192a75468c89c7790a408c4cef2989
Mayusha <100788991+Mayusha-R@users.noreply.github.com>
2025-01-10 15:04:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,4 +6,21 @@ variable "vm_username" {}
 variable "vnet_range" {}
 variable "subnet_range" {}
 variable "network_interface_name" {}
-
+variable "resource_owner" {
+  type = string
+}
+variable "business_unit" {
+  type = string
+}
+variable "project_name" {
+  type = string
+}
+variable "delivery_manager" {
+  type = string
+}
+variable "create_date" {
+  type = string
+}
+variable "key_name" {
+  type = string
+}
